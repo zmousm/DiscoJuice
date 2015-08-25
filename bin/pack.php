8eaeb6387f3e8d6a240792222be79862f656ce36
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2015-08-25 20:47:57 +0300

    Introduce the java compiler version of the packJS (subordinate) function.
    STDERR from the compiler goes to builds/stderr.log

--- bin/pack.php
+++ bin/pack.php
@@ -24,6 +24,40 @@ function packJSapi($data, $input_file) {
 	return $compressed;
 }
 
+function packJScompiler($data, $input_file = null) {
+  global $basename;
+  if (is_string($input_file)) {
+    $stderr = dirname($input_file) . '/stderr.log';
+  } else {
+    $stderr = $basename . '/stderr.log';
+  }
+  $descriptorspec = array(
+			  0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+			  1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+			  2 => array("file", $stderr, "a") // stderr is a file to write to
+			  );
+  $opts = array(
+		'--compilation_level', 'SIMPLE',
+		'--warning_level', 'DEFAULT',
+		);
+  if (is_string($input_file)) {
+    $opts = array_merge($opts, array('--js', $input_file));
+    $descriptorspec[0] = array("file", "/dev/null", "r");
+  }
+  $cmdline = array_merge(explode(' ', "java -jar $basename/compiler.jar"), $opts);
+  file_put_contents($stderr, implode(' ', array_merge($cmdline, array("\n\n"))), FILE_APPEND);
+  $process = proc_open(implode(' ', $cmdline), $descriptorspec, $pipes);
+  if (is_resource($process)) {
+    if (!is_string($input_file)) {
+      fwrite($pipes[0], $data);
+      fclose($pipes[0]);
+    }
+    $compressed = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $return_value = proc_close($process);
+  }
+  return $compressed;
+}
 function packJS($data, $filename) {
   $closure_input_filename = preg_replace("#\.min#", "", $filename);
   file_put_contents($closure_input_filename, $data);
